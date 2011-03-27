816149dea4bb37864b9fae4c2dcd387091c195e2
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-03-26 21:35:40 -0300

    Added a delegate example

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,10 @@
 class Bookmark < ActiveRecord::Base
   validates_presence_of :url
+
+  delegate :scheme, :host, :path, :to => :parsed_url
+
+  def parsed_url
+    @uri ||= URI.parse(url)
+    @uri
+  end
 end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -2,4 +2,32 @@ require 'spec_helper'
 
 describe Bookmark do
   it { should validate_presence_of :url }
+
+  describe '#schema' do
+    it "should return 'http' we have a http schema" do
+      url = Bookmark.create(:url => "http://www.google.com")
+      url.scheme.should == "http"
+    end
+
+    it "should return 'https' when we have a https schema" do
+      url = Bookmark.create(:url => "https://www.facebook.com")
+      url.scheme.should == "https"
+    end
+
+    describe '#host' do
+      it "should return 'www.google.com' if we have a http://www.google.com url" do
+        url = Bookmark.create(:url => "http://www.google.com")
+        url.host.should == "www.google.com"
+      end
+    end
+
+    describe '#path' do
+      it "should return '/reader' if we have a http://www.google.com/reader" do
+        url = Bookmark.create(:url => "http://www.google.com/reader")
+        url.path.should == "/reader"
+      end
+
+    end
+
+  end
 end
