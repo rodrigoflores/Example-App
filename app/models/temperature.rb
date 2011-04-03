764e00964929ff21614c2c7c3139d9e73969ebc9
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-04-02 21:16:59 -0300

    Delegate !

--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -1,5 +1,5 @@
 class Temperature < ActiveRecord::Base
   validates_presence_of :temperature_in_celsius
   composed_of :temperature_converter, :mapping => %w(temperature_in_celsius temperature)
-
+  delegate :in_fahrenheit, :in_celsius, :in_kelvin, :to => :temperature_converter
 end
