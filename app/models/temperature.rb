7265da9660bf980fd14ba8b3fc7f131f8277b65e
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-04-02 21:03:31 -0300

    Renaming some methods

--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -1,11 +1,11 @@
 class Temperature < ActiveRecord::Base
   validates_presence_of :temperature_in_celsius
 
-  def temperature_in_fahrenheit
+  def in_fahrenheit
     (9.0/5.0)*temperature_in_celsius + 32.0
   end
 
-  def temperature_in_kelvin
+  def in_kelvin
     temperature_in_celsius + 273.15
   end
 end
