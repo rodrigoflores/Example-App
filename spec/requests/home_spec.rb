8b0ede0aa0a7de044826415be1bc24eba9dc5de8
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-04-04 22:17:02 -0300

    Added authorizations tests

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -5,7 +5,33 @@ describe 'visiting the homepage' do
     visit '/'
   end
 
-  it 'should have a body' do
-    page.should have_css('body')    
+  it 'should have a facebook link' do
+    page.should have_css("a[href='/auth/facebook']", :text => "Facebook")
   end
+
+  it 'should log me in when I click on the facebook link' do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:facebook] = {
+      'uid' => '123545',
+      'provider' => 'facebook',
+      'user_info' => { 'first_name' => "Elizabeth", 'last_name' => "Hurley"}
+    }
+    click_link "Facebook"
+    page.should have_css("h1", :text => "Welcome Elizabeth Hurley")
+    OmniAuth.config.test_mode = false
+  end
+
+  it 'should log me out when I click on the log out icon' do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:facebook] = {
+      'uid' => '123545',
+      'provider' => 'facebook',
+      'user_info' => { 'first_name' => "Elizabeth", 'last_name' => "Hurley"}
+    }
+    click_link "Facebook"
+    click_link "Log out"
+    page.should have_css("a[href='/auth/facebook']", :text => "Facebook")
+    OmniAuth.config.test_mode = false
+  end
+
 end
